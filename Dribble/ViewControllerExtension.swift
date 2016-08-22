c2a7f6d511e006acb5b5689a72dc44c20298a2d3
Imsiaocong <wd1998313@gmail.com>
2016-08-22 06:29:28 -0400

    Add UIBezeierPath To ViewController

--- Dribble/ViewControllerExtension.swift
+++ Dribble/ViewControllerExtension.swift
@@ -10,12 +10,23 @@ import UIKit
 
 extension ViewController{
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        
+        let shapeLayer = CAShapeLayer()
+        let mainPath1 = UIBezierPath()
+        let getMid = self.view.frame.size.width/2
         //print(scrollView.contentOffset.x)
         //pageControl.scrollWithScrollView(collectionView)
+        if scrollView.contentOffset.x < 0 {
+            shapeLayer.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width/2, height: self.view.frame.size.height)
+            shapeLayer.fillColor = UIColor.blueColor().CGColor
+            mainPath1.moveToPoint(CGPoint(x: 0, y: 0))
+            mainPath1.addCurveToPoint(CGPointMake(0, self.view.frame.size.height), controlPoint1: CGPointMake(getMid, 0), controlPoint2: CGPointMake(getMid*1.5, 0))
+            shapeLayer.path = mainPath1.CGPath
+            self.view.layer.insertSublayer(shapeLayer, atIndex: 0)
+        }
         
         if scrollView.contentOffset.x > 0 && scrollView.contentOffset.x <= 180{
             self.backgroundPic.image = UIImage(named: "0")
+            mainPath1.removeAllPoints()
         }
         if scrollView.contentOffset.x > 180 && scrollView.contentOffset.x <= 560{
             self.backgroundPic.image = UIImage(named: "1")
